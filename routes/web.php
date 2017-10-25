9d49a0ced2447b52fdba5d4ebaa0ef7ce84b36b8
許漢昌 <s3a432053@student.ncut.edu.tw>
2017-10-25 19:01:43 +0800

    修改第一個路由

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    //利用model Task由DB的tasks資料表取出資料
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
 
 // 增加新的任務

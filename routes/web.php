cf4f3ccb16e6c927b03f8e899df38c83f1480c8d
許漢昌 <s3a432053@student.ncut.edu.tw>
2017-10-25 18:59:47 +0800

    第二個路由‘/task’ ：用以新增任務 & 驗證輸入內容

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,19 @@ Route::get('/', function () {
 
 // 增加新的任務
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
 });
 
 // 刪除任務
